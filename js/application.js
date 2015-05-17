9d1775ffb69fa01eb8524cd57fa5b7fb71e50977
Sean Soper <sean.soper@gmail.com>
2015-05-16 20:46:02 -0400

    wordcloud

--- js/application.js
+++ js/application.js
@@ -1,5 +1,6 @@
 var showEntryByIdx = function(idx) {
     var content = window.content[idx];
+    $('#wordcloud').remove();
     $('.content:first').html(content.html);
     $('.navigation').css({ display: 'block' });
 
@@ -48,4 +49,47 @@ window.showEntryByDate = function(dateStr) {
 
 $(document).ready(function() {
     $(document).scrollTop(0);
+
+    window.cloudWords = [];
+    var smallestCount = window.topWords[window.topWords.length-1][1];
+    window.topWords.forEach(function(wordAry) {
+        var word = wordAry[0];
+        var count = wordAry[1];
+        count = parseInt(count/smallestCount);
+        console.log(smallestCount + ':' + wordAry[1] + ':' + count);
+
+        for(i=0; i < count; i++) {
+            window.cloudWords.push(word);
+        }
+    });
+
+    var fill = d3.scale.category20();
+      d3.layout.cloud().size([960, 600])
+          .words(window.cloudWords.map(function(d) {
+            return {text: d, size: 10 + Math.random() * 90};
+          }))
+          .padding(5)
+          .rotate(function() { return ~~(Math.random() * 2) * 90; })
+          .font("Impact")
+          .fontSize(function(d) { return d.size; })
+          .on("end", draw)
+          .start();
+      function draw(words) {
+        d3.select("#wordcloud").append("svg")
+            .attr("width", 960)
+            .attr("height", 600)
+          .append("g")
+            .attr("transform", "translate(480,300)")
+          .selectAll("text")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+      }
 });
